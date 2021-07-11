3ab0782c35a924cd89956cf22e297819d1bd0a8a
MKCrossMK <mikydelacruz116@gmail.com>
2021-07-11 00:25:22 -0300

    refactor Cors

--- app/Http/Middleware/cors.php
+++ app/Http/Middleware/cors.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 
-class cors
+class Cors
 {
     /**
      * Handle an incoming request.
@@ -22,5 +22,6 @@ class cors
     header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
     header('Access-Control-Max-Age: 1000');
     header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+
     }
 }
